49e100c075b3dcbd168f8d1ad60dcb0a75776af7
pawelek3113 <pawel.kamil.komendera@gmail.com>
2023-01-16 21:14:20 +0100

    Changing status

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import nextcord
-from nextcord.ext import commands
+from nextcord.ext import commands, tasks
 import os
 from dotenv import load_dotenv
+from itertools import cycle
 
 load_dotenv()
+
+
+@tasks.loop(seconds=15)
+async def change_status(bot):
+    await bot.change_presence(status=nextcord.Status.idle, activity=nextcord.Game(next(Bot.bot_status)))
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
@@ -12,14 +18,14 @@ class Bot(commands.Bot):
             if file.endswith(".py"):
                 self.load_extension(f"modules.{file[:-3]}")
 
+    bot_status = cycle(["with yo mama", "with yo papa"])
+    
     async def on_ready(self):
         print(f"Logged in as {self.user.name}, ID: {self.user.id}")
-        await self.change_presence(status=nextcord.Status.idle, activity=nextcord.Game("with yo mama"))
-    
-    
-        
+        change_status.start(self)
+
+
 intents = nextcord.Intents.all()
-# intents.message_content = True
 bot = Bot(command_prefix="!", case_insensitive=True, intents=intents)
 
 bot.run(os.environ["TOKEN"])
